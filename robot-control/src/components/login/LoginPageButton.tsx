d4811ce8d8c6cd0a1686193b8101d4b9410d60db
Jacob Crewe <jacobdc0602@gmail.com>
2024-01-02 17:07:29 -0500

    Added logout and ckind of formatting

--- robot-control/src/components/login/LoginPageButton.tsx
+++ robot-control/src/components/login/LoginPageButton.tsx
@@ -4,13 +4,18 @@ export interface LoginButtonProps {
   prompt: any;
   onClick?: () => void;
   id?: string;
+  bgColor?: string;
+  textColor?: string;
 }
 
 export const LoginPageButton = (props: LoginButtonProps) => {
   return (
     <button
       type="submit"
-      className="shadow-[0px_0px_10px_rgba(0,0,0,0.4)] hover:shadow-[0px_0px_10px_rgba(0,0,0,0.6)] whitespace-nowrap rounded-full text-white w-4/5 bg-indigo-900 brightness-[0.95] hover:brightness-100 px-6 py-2 scale-100 hover:scale-[1.01] transition-transform ease-in-out duration-150"
+      className={`shadow-[0px_0px_10px_rgba(0,0,0,0.4)] hover:shadow-[0px_0px_10px_rgba(0,0,0,0.6)] whitespace-nowrap rounded-full w-4/5 
+      ${!props.bgColor ? "bg-indigo-900" : props.bgColor} 
+      ${!props.textColor ? "text-white" : props.textColor}
+      brightness-[0.95] hover:brightness-100 px-6 py-2 scale-100 hover:scale-[1.01] transition-transform ease-in-out duration-150`}
       onClick={props.onClick}
     >
       {props.prompt}
--- robot-control/src/components/pages/Dashboard.tsx
+++ robot-control/src/components/pages/Dashboard.tsx
@@ -1,18 +1,33 @@
 import React, { useContext } from "react";
 import { UserContext } from "../ContentRouter.tsx";
 import img from "../../assets/images/sample.png";
+import { LoginPageButton } from "../login/LoginPageButton.tsx";
 
 export function Dashboard() {
-  const { login } = useContext(UserContext);
+  const { login, setLogin } = useContext(UserContext);
 
   return (
-    <div className="w-full h-full min-w-[330px] overscroll-x-contain">
-      <div className="w-full h-full flex flex-row gap-4">
-        <div className="rounded-xl overflow-hidden aspect-[4/3] bg-white h-full">
-          <img src={img} className="w-full h-full" />
-        </div>
-        <div className="rounded-xl p-4 bg-white overflow-hidden shadow-outline md:col-span-2">
-          FILLER
+    <div className="w-full h-full min-w-[330px] overscroll-x-contain overscroll-y-none">
+      <div className="w-full h-full flex flex-wrap gap-4 justify-center">
+        <img
+          src={img}
+          className="rounded-xl overflow-hidden [4/3] shadow-outline object-cover"
+        />
+
+        <div className="flex-grow p-4 min-w-32 min-h-16">
+          <div className="w-full h-full relative">
+            <div className="absolute flex bottom-0 mt-auto w-full items-center justify-center">
+              <LoginPageButton
+                prompt="Logout"
+                bgColor="bg-neutral-300"
+                textColor="text-black"
+                onClick={() => {
+                  setLogin("");
+                  localStorage.removeItem("userLogin");
+                }}
+              />
+            </div>
+          </div>
         </div>
       </div>
     </div>
