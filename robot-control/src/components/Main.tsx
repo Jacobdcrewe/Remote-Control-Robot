d269d641e642f3c5230dedc618341841ef69d812
Jacob Crewe <jacobdc0602@gmail.com>
2024-01-04 00:27:51 -0500

    :Added mostly functional frontend

--- robot-control/src/components/Main.tsx
+++ robot-control/src/components/Main.tsx
@@ -4,7 +4,7 @@ import ContentRouter from "./ContentRouter.tsx";
 export function Main() {
   return (
     <div className="w-screen h-screen bg-black overflow-hidden flex text-neutral-950 ">
-      <div className="flex flex-grow items-center justify-center bg-gradient-to-t from-black to-indigo-950 overflow-hidden p-4">
+      <div className="flex flex-grow items-center justify-center bg-gradient-to-t from-black to-indigo-950  overflow-x-hidden overflow-y-auto p-4">
         <ContentRouter />
       </div>
     </div>
--- robot-control/src/components/pages/Dashboard.tsx
+++ robot-control/src/components/pages/Dashboard.tsx
@@ -1,31 +1,102 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { UserContext } from "../ContentRouter.tsx";
 import img from "../../assets/images/sample.png";
 import { LoginPageButton } from "../login/LoginPageButton.tsx";
+import { ArrowLeftIcon } from "@heroicons/react/24/solid";
+import {
+  ArrowDownIcon,
+  ArrowRightIcon,
+  ArrowUpIcon,
+} from "@heroicons/react/24/outline";
 
 export function Dashboard() {
   const { login, setLogin } = useContext(UserContext);
+  const [pictureLeft, setPictureLeft] = useState(true);
+  console.log(pictureLeft);
+
+  const handleLeft = () => {
+    console.log("left");
+  };
+
+  const handleRight = () => {
+    console.log("right");
+  };
+
+  const handleUp = () => {
+    console.log("up");
+  };
+
+  const handleDown = () => {
+    console.log("down");
+  };
 
   return (
-    <div className="w-full h-full min-w-[330px] overscroll-x-contain overscroll-y-none">
+    <div className="w-full h-full min-w-[330px] overscroll-x-contain">
       <div className="w-full h-full flex flex-wrap gap-4 justify-center">
         <img
           src={img}
-          className="rounded-xl overflow-hidden [4/3] shadow-outline object-cover"
+          className={`${
+            pictureLeft ? "order-1" : "order-2"
+          } rounded-xl overflow-hidden aspect-[4/3] max-h-full shadow-outline object-cover`}
         />
 
-        <div className="flex-grow p-4 min-w-32 min-h-16">
-          <div className="w-full h-full relative">
-            <div className="absolute flex bottom-0 mt-auto w-full items-center justify-center">
-              <LoginPageButton
-                prompt="Logout"
-                bgColor="bg-neutral-300"
-                textColor="text-black"
-                onClick={() => {
-                  setLogin("");
-                  localStorage.removeItem("userLogin");
-                }}
-              />
+        <div
+          className={`${
+            pictureLeft ? "order-2" : "order-1"
+          } flex-grow p-4 min-w-32 min-h-16 `}
+        >
+          <div className="w-full h-full relative flex flex-row xl:flex-col ">
+            <div className="flex aspect-square mx-auto xl:w-4/5 2xl:w-3/5 grid grid-cols-3 gap-4 grid-rows-3">
+              <button
+                onClick={handleUp}
+                className="flex items-center justify-center col-start-2 bg-neutral-400 rounded-xl shadow-outline opacity-50 m-px hover:m-0 hover:cursor-pointer hover:opacity-85 transition-all ease-in-out duration-150"
+              >
+                <ArrowUpIcon className="w-1/2 h-1/2 m-auto text-neutral-100" />
+              </button>
+              <button
+                onClick={handleLeft}
+                className="flex items-center justify-center row-start-2 col-start-1  bg-neutral-400 rounded-xl shadow-outline opacity-50 m-px hover:m-0 hover:cursor-pointer hover:opacity-85 transition-all ease-in-out duration-150"
+              >
+                <ArrowLeftIcon className="w-1/2 h-1/2 m-auto text-neutral-100" />
+              </button>
+              <button
+                onClick={handleRight}
+                className="flex items-center justify-center row-start-2 col-start-3  bg-neutral-400 rounded-xl shadow-outline opacity-50 m-px hover:m-0 hover:cursor-pointer hover:opacity-85 transition-all ease-in-out duration-150"
+              >
+                <ArrowRightIcon className="w-1/2 h-1/2 m-auto text-neutral-100" />
+              </button>
+              <button
+                onClick={handleDown}
+                className="flex items-center justify-center row-start-3 col-start-2  bg-neutral-400 rounded-xl shadow-outline opacity-50 m-px hover:m-0 hover:cursor-pointer hover:opacity-85 transition-all ease-in-out duration-150"
+              >
+                <ArrowDownIcon className="w-1/2 h-1/2 m-auto text-neutral-100" />
+              </button>
+            </div>
+            <div className="m-auto xl:mt-auto xl:mb-0 w-full">
+              <div className="inline-flex gap-4 text-neutral-100 w-full justify-center p-4 text-nowrap">
+                Right Handed
+                <label className="relative inline-flex items-center cursor-pointer">
+                  <input
+                    type="checkbox"
+                    value=""
+                    className="sr-only peer"
+                    onClick={() => setPictureLeft(!pictureLeft)}
+                  />
+                  <div className="w-14 h-7 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:start-[4px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-6 after:w-6 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+                </label>
+                Left Handed
+              </div>
+              <div className="flex w-full items-center justify-center">
+                <LoginPageButton
+                  prompt="Logout"
+                  bgColor="bg-neutral-300"
+                  textColor="text-black"
+                  onClick={() => {
+                    setLogin("");
+                    localStorage.removeItem("userLogin");
+                  }}
+                />
+              </div>
             </div>
           </div>
         </div>
